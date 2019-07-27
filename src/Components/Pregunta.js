63dfc8f04172dd2f73d62407c9654f860cd9ca9c
victor hugo <victor03016@gmail.com>
2019-07-27 19:48:26 +0200

    Creacion Componente Pregunta
    Creación del componente pregunta que insertararemos en App principal

---
+++ src/Components/Pregunta.js
@@ -0,0 +1,24 @@
+import React, { Fragment } from 'react';
+
+function Pregunta() {
+    return (
+        <Fragment>
+            <h2>Colaca tu Presupuesto</h2>
+            <form>
+                <input 
+                    type="number" 
+                    className="u-full-width"
+                    placeholder="Agrega tu Presupuesto"
+                    onChange={}
+                />
+                <input 
+                    type="submit" 
+                    className="button-primary u-full-width" 
+                    value="Definir Presupuesto" 
+                />
+            </form>
+        </Fragment>
+    );
+}
+
+export default Pregunta;
