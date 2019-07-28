5250b8d5745df9188b973c3fcd6d5d73f36eed69
victor hugo <victor03016@gmail.com>
2019-07-28 12:58:34 +0200

    Añadiendo icono a traves de materials
    Añadir icono para eliminar un gasto

--- src/Components/Gasto.js
+++ src/Components/Gasto.js
@@ -1,10 +1,38 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import SvgIcon from "@material-ui/icons/DeleteForever";
+import { blue } from "@material-ui/core/colors";
 
-const Gasto = (props) => {
-    
+const useStyles = makeStyles(theme => ({
+    root: {
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "flex-end"
+    },
+    icon: {
+        margin: theme.spacing(2)
+    },
+    iconHover: {
+        margin: theme.spacing(2),
+        "&:hover": {
+            color: blue[800]
+        }
+    }
+}));
+
+function HomeIcon(props) {
+    return (
+        <SvgIcon {...props}>
+            <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+        </SvgIcon>
+    );
+}
+
+const Gasto = props => {
+    const classes = useStyles();
+
     const { gasto, setIdGasto, setEliminado } = props;
 
-    
     const eliminaGasto = id => {
         // console.log(id);
         setEliminado(true);
@@ -16,9 +44,12 @@ const Gasto = (props) => {
             <p>
                 {gasto.nombreGasto}
                 <span className="gasto">$ {gasto.cantidadGasto}</span>
-                <button type="button" onClick={() => eliminaGasto(gasto.id)}>
-                    Eliminar
-                </button>
+                <HomeIcon
+                    className={classes.iconHover}
+                    color="error"
+                    style={{ fontSize: 35 }}
+                    onClick={() => eliminaGasto(gasto.id)}
+                />
             </p>
         </li>
     );
